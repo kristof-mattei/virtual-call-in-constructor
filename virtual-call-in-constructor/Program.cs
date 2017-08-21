1e265d1d98ff82b987c069f5d196e9e2e33fd441
Kristof Mattei <csharpfan@users.noreply.github.com>
2017-08-20 20:56:27 -0700

    Add project files.

---
+++ virtual-call-in-constructor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_call_in_constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
